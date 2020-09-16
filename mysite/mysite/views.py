5cc942253a953099eb8b1c8db2379f0a18ca2416
Eterna-E <o23306559@gmail.com>
2020-09-16 21:12:08 +0800

    second commit

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -20,5 +20,6 @@ def math(request, a, b): # 加入
 def menu(request):
     food1 = {'name': '番茄炒蛋', 'price': 60, 'comment': '好吃', 'is_spicy': False}
     food2 = {'name': '蒜泥白肉', 'price': 100, 'comment': '人氣推薦', 'is_spicy': True}
+    foody = food1.items()
     foods = [food1, food2]
     return render(request,'menu.html', locals())
